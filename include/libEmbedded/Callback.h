ed33a0e1a23a9247e65269b2cf057977561dafdc
GWillemsen <33036522+GWillemsen@users.noreply.github.com>
2022-03-13 18:21:26 +0100

    Simple Callback with some tests

--- include/libEmbedded/Callback.h
+++ include/libEmbedded/Callback.h
@@ -2,8 +2,9 @@
  * @file Callback.h
  * @author Giel Willemsen
  * @brief Wrapper for a function pointer with a userdata parameter.
- * @version 0.1 2022-02-07
- * @date 2022-02-07
+ * @version 0.1 2022-02-07 Initial version
+ * @version 0.1 2022-03-09 Addition of specific Invoke method.
+ * @date 2022-03-09
  *
  * @copyright Copyright (c) 2022
  *
@@ -19,12 +20,12 @@ namespace libEmbedded
      *
      * @tparam TCallback The function pointer for this callback.
      */
-    template <typename TCallback, typename TReturnType = void>
+    template <typename TCallback, typename TContext = void, typename TReturnType = void>
     class Callback
     {
     private:
         TCallback call;
-        void *usrdata;
+        TContext *usrdata;
 
     public:
         /**
@@ -39,7 +40,7 @@ namespace libEmbedded
          * @param callback The callback to preload the instance with.
          * @param userdata The optional userdata for the callback when called.
          */
-        constexpr Callback(TCallback callback, void *userdata) : call(callback), usrdata(userdata) {}
+        constexpr Callback(TCallback callback, TContext *userdata) : call(callback), usrdata(userdata) {}
 
         /**
          * @brief Check if the callback is set and can be called or not.
@@ -55,7 +56,16 @@ namespace libEmbedded
          * @param callback The callback to handle the invoke.
          * @param userdata Optional userdata that is given to the callback when called.
          */
-        void SetCallback(TCallback callback, void *userdata = nullptr);
+        void SetCallback(TCallback callback, TContext *userdata = nullptr);
+
+        /**
+         * @brief Invoke the callback itself.
+         *
+         * @tparam TArgs The type of the arguments for the callback.
+         * @param args The arguments for the callback (excluding the userdata).
+         */
+        template <typename... TArgs>
+        TReturnType Invoke(TArgs... args) const;
 
         /**
          * @brief Invoke the callback itself.
@@ -67,28 +77,35 @@ namespace libEmbedded
         TReturnType operator()(TArgs... args) const;
     };
     
-    template <typename TCallback, typename TReturnType>
-    bool Callback<TCallback, TReturnType>::IsSet() const
+    template <typename TCallback, typename TContext, typename TReturnType>
+    bool Callback<TCallback, TContext, TReturnType>::IsSet() const
     {
         return this->call != nullptr;
     }
 
-    template <typename TCallback, typename TReturnType>
-    void Callback<TCallback, TReturnType>::SetCallback(TCallback callback, void *userdata)
+    template <typename TCallback, typename TContext, typename TReturnType>
+    void Callback<TCallback, TContext, TReturnType>::SetCallback(TCallback callback, TContext *userdata)
     {
         this->call = callback;
         this->usrdata = userdata;
     }
 
-    template <typename TCallback, typename TReturnType>
+    template <typename TCallback, typename TContext, typename TReturnType>
     template <typename... TArgs>
-    TReturnType Callback<TCallback, TReturnType>::operator()(TArgs... args) const
+    TReturnType Callback<TCallback, TContext, TReturnType>::Invoke(TArgs... args) const
     {
         if (this->call != nullptr)
         {
-            return this->call(this->userdata, args...);
+            return this->call(this->usrdata, args...);
         }
         return TReturnType();
+    }
+
+    template <typename TCallback, typename TContext, typename TReturnType>
+    template <typename... TArgs>
+    TReturnType Callback<TCallback, TContext, TReturnType>::operator()(TArgs... args) const
+    {
+        return Invoke(args...);
     }
 } // namespace libEmbedded
 
