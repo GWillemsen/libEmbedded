1ecbcc2f58600860ff6787885737153ddf8485da
Giel Willemsen <giel.willemsen@intemo.com>
2022-08-26 21:03:13 +0200

    Added descriptions

--- include/libEmbedded/Expected.h
+++ include/libEmbedded/Expected.h
@@ -1,7 +1,7 @@
 /**
  * @file Expected.h
  * @author Giel Willemsen
- * @brief Represents a value that is usually present but sometimes it is not, and then adfana other type of value is given.
+ * @brief Represents a value that is usually present but sometimes it is not, and then another type of value is given.
  * @version 0.1
  * @date 2022-07-26
  * 
@@ -15,6 +15,14 @@
 
 namespace libEmbedded
 {
+
+    /**
+     * @brief Represent a value that usually present but a alternative value might be given instead.
+     * WARNING: Accessors don't check if a given value is set. You need to check that a value is set, and if it is set, if it is a expected or alternative value.
+     * 
+     * @tparam TExpected 
+     * @tparam TAlternative 
+     */
     template<typename TExpected, typename TAlternative>
     class Expected
     {
@@ -29,6 +37,12 @@ namespace libEmbedded
         bool value_set;
 
     public:
+
+        /**
+         * @brief Construct a new Expected object from a existing one.
+         * 
+         * @param other The other Expected object to copy.
+         */
         Expected(const Expected& other) : value(), is_expected(other.is_expected), value_set(other.value_set)
         {
             if (!other.value_set)
@@ -46,6 +60,12 @@ namespace libEmbedded
             }
         }
 
+        /**
+         * @brief Copy the value from the given Expected to the current and return the updated current.
+         * 
+         * @param other The other Expected to copy.
+         * @return Expected& The current Expected itself.
+         */
         Expected& operator=(const Expected& other)
         {
             if (this->value_set)
@@ -72,41 +92,83 @@ namespace libEmbedded
             return *this;
         }
 
+        /**
+         * @brief Destroy this Expected object.
+         * 
+         */
         ~Expected()
         {
             this->DestructValue();
         }
 
+        /**
+         * @brief Get the expected value.
+         * 
+         * @return const TExpected& The current value as a expected value.
+         */
         const TExpected& GetExpected() const
         {
             return *GetPtr<TExpected>();
         }
 
+        /**
+         * @brief Get the expected value.
+         * 
+         * @return const TExpected& The current value as a expected value.
+         */
         TExpected& GetExpected()
         {
             return *GetPtr<TExpected>();
         }
 
+        /**
+         * @brief Get the Alternative object.
+         * 
+         * @return const TAlternative& The current value as a alternative value.
+         */
         const TAlternative& GetAlternative() const
         {
             return *GetPtr<TAlternative>();
         }
 
+        /**
+         * @brief Get the Alternative object.
+         * 
+         * @return const TAlternative& The current value as a alternative value.
+         */
         TAlternative& GetAlternative()
         {
             return *GetPtr<TAlternative>();
         }
 
+        /**
+         * @brief The value that is set is of the Expected type.
+         * 
+         * @return true The value that is set is a Expected value.
+         * @return false Either there is no value set, or the value that is set is of the Alternative type.
+         */
         bool HasExpected() const
         {
             return this->is_expected;
         }
 
+        /**
+         * @brief Check if this object has a value.
+         * 
+         * @return true If a value is set.
+         * @return false If there is no value set.
+         */
         bool HasValue() const
         {
             return this->value_set;
         }
 
+        /**
+         * @brief Construct a new Expected object from the a TExpected value.
+         * 
+         * @param expected The value to construct the expected value from.
+         * @return Expected The resulting Expected object.
+         */
         static Expected FromExpected(const TExpected& expected)
         {
             Expected exp;
@@ -114,6 +176,12 @@ namespace libEmbedded
             return exp;
         }
 
+        /**
+         * @brief Construct a new Expected object from the a TAlternative value.
+         * 
+         * @param expected The value to construct the expected value from.
+         * @return Expected The resulting Expected object.
+         */
         static Expected FromAlternative(const TAlternative& alternative)
         {
             Expected exp;
